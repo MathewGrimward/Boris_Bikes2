4b7c8d23ae1dff231fc07b3dfd3f775571832c84
Mathew Grimward <mathewgrimward@gmail.com>
2022-01-20 14:07:26 +0000

    Challenge Current

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,25 @@
-class DockingStation
-  def release_bike
+require_relative 'bike'
 
+class DockingStation
+  attr_reader :bike
+
+  def initialize
+    @docked = []
   end
+
+  def dock_bike(bike)
+    @docked << bike
+    "Bike docked!"
+  end
+
+  def release_bike
+    fail 'No bikes available' unless 
+    @bike
+    Bike.new
+  end
+
+  def dock(bike)
+    @bike = bike
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,19 +1,39 @@
 require './lib/docking_station.rb'
 
   describe DockingStation do
-    it { is_expected.to respond_to "release_bike"}
-
-=begin
-    it 'release bike' do
-      respond_to?(subject.release_bike)
+    it 'releases working bikes' do
+      bike = subject.release_bike
+      expect(bike).to be_working
     end
-=end 
 
-=begin
-    it 'releases bike' do
+    it { is_expected.to respond_to(:dock).with(1).argument }
+
+    it 'can dock a bike' do
       docking_station = DockingStation.new
-      expect(docking_station.release_bike).to respond_to 'Bike Released!'
+      docking_station.dock_bike('bike1')
+      expect(docking_station.dock_bike('bike1')).to eq "Bike docked!"
     end
-=end
-    
-end
+
+    it 'docks something' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.dock(bike)).to eq bike
+    end
+  end
+  
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      # we want to release the bike we docked
+      expect(subject.release_bike).to eq bike
+  end
+  
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+  end
+
+    #it 'release bike' do
+    #  respond_to?(subject.release_bike)
+    #end
